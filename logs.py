a1935f553eadb7ca4df45aebb7481d12e05117c0
Abhishek <abhi736902@gmail.com>
2025-10-04 11:56:26 +0530

    Update logs.py

--- logs.py
+++ logs.py
@@ -1,7 +1,3 @@
-# Don't Remove Credit Tg - https://t.me/roxybasicneedbot1
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@roxybasicneedbot
-# Ask Doubt on telegram https://t.me/roxybasicneedbot1
-
 import logging
 from logging.handlers import RotatingFileHandler
 
@@ -19,4 +15,5 @@ logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
 
 logging = logging.getLogger()
+
 
